f368d0e8102db512541a904e97a821dd7c2a99a2
Nithursan <nisi20ac@student.cbs.dk>
2020-10-18 17:39:40 +0200

    tilføjet model-klasserne

---
+++ Model/paymentuser.js
@@ -0,0 +1,3 @@
+class PaymentUser {
+    constructor
+}
---
+++ Model/user.js
@@ -0,0 +1,15 @@
+class User {
+    constructor(firstname, lastname, age, city, bio, password, image, interest) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.city = city;
+        this.bio = bio;
+        this.password = password;
+        this.image = image;
+        this.interest = interest;
+
+    }
+}
+
+module.exports = User;
